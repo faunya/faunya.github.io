7af7e3342d5204fec9ad0eb32266ce805e5c5736
jay <bayonetm8@gmail.com>
2026-01-09 14:11:37 -0500

    react router, chnage color on hover over links in navbar

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -4,7 +4,7 @@ export default function WebNavBar() {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
-        <li><a href="">Home</a></li>
+        <li><a href="#">Home</a></li>
         <li><a href="">About</a></li>
         <li><a href="">Projects</a></li>
         <li><a href="">Resume</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import WebNavBar from "./navbar/navbar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex font-sans dark:bg-black">
       <WebNavBar />
     </div>
   );
