8b82c7dcede434333a3d6ec537be9d1884091185
jay <bayonetm8@gmail.com>
2026-01-09 17:01:43 -0500

    changed to vite framework

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import WebNavBar from "./navbar/navbar";
-
-export default function Home() {
-  return (
-    <div className="flex font-sans dark:bg-black">
-      <WebNavBar />
-    </div>
-  );
-}
